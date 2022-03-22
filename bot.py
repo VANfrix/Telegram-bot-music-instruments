e79e9f2faed7a15de96ec4dfb0c92f072cbbc46e
VANfrix <ultrafrost2012@yandex.ru>
2022-03-22 23:23:40 +0300

    Парсинг авито

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
+from ast import Store
+from distutils.ccompiler import show_compilers
 import logging
 from token_bot import bot_token
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, Filters
-
+from avito import get_avito
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 def start(update: Update, context: CallbackContext):
@@ -14,10 +16,16 @@ def start(update: Update, context: CallbackContext):
     text_greeting = "Привет, я бот для поиска объявлений по продаже гитар!\nЧто я умею:\n- искать гитару по определённым характеристикам\n- присылать новые объявления, нужно только меня об этом попросить)))\n- если ждёшь гитару мечты, я пришлю обявление, когда появится!"
     update.message.reply_text(text_greeting, reply_markup=my_keyboard)
 
+guitars = get_avito()
 
 def search(update: Update, context: CallbackContext):
-    update.message.reply_text('Здесь будет осуществляться поиск инструмента')
-
+    for guitar in guitars:
+        name_=guitar['name_guitar']
+        price_=guitar['price_guitar']
+        saler_=guitar['name_saler']
+        search_new = f'{name_} цена {price_} продаёт {saler_}'
+        update.message.reply_text(search_new)
+    
 def news(update: Update, context: CallbackContext):
     update.message.reply_text('Здесь я буду присылать новые объявления')
 
