eeb4a577a579e0d7f0fb2f0b07159d4ed08390a9
VANfrix <99546594+VANfrix@users.noreply.github.com>
2022-03-26 23:34:17 +0300

    Парсинг авито (#2)
    * Парсинг авито
    
    * Исправление ошибок по парсингу авито
    
    * Доработка по парсингу+кнопки Далее

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import logging
 from token_bot import bot_token
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, Update
+from telegram import ReplyKeyboardMarkup, Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, Filters
-
+from avito import get_avito
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 def start(update: Update, context: CallbackContext):
@@ -14,10 +14,33 @@ def start(update: Update, context: CallbackContext):
     text_greeting = "Привет, я бот для поиска объявлений по продаже гитар!\nЧто я умею:\n- искать гитару по определённым характеристикам\n- присылать новые объявления, нужно только меня об этом попросить)))\n- если ждёшь гитару мечты, я пришлю обявление, когда появится!"
     update.message.reply_text(text_greeting, reply_markup=my_keyboard)
 
+def get_guitar_message(pages):
+    guitars = get_avito()
+    dict_list = ''
+    for guitar in guitars[pages:pages+10]:
+        name_=guitar['name_guitar']
+        price_=guitar['price_guitar']
+        saler_=guitar['name_saler']
+        place_=guitar['place_guitar']
+        search_new = f'\t{name_}\n цена {price_}\n продаёт {saler_}\n место {place_}\n\n'
+        dict_list += search_new
+    return dict_list
 
 def search(update: Update, context: CallbackContext):
-    update.message.reply_text('Здесь будет осуществляться поиск инструмента')
+    pages = 0
+    dict_list = get_guitar_message(pages)
+    # print(len(dict_list))
+    context.user_data["pages"] = pages+10
+    update.message.reply_text(dict_list, reply_markup= ReplyKeyboardMarkup([['Далее']]))
 
+def search_next(update: Update, context: CallbackContext):
+    pages = context.user_data["pages"]
+    dict_list = get_guitar_message(pages)
+    # print(len(dict_list))
+    context.user_data["pages"] = pages+10
+    update.message.reply_text(dict_list, reply_markup= ReplyKeyboardMarkup([['Далее']]))
+    
+    
 def news(update: Update, context: CallbackContext):
     update.message.reply_text('Здесь я буду присылать новые объявления')
 
@@ -37,6 +60,7 @@ def main():
     dp.add_handler(MessageHandler(Filters.regex('^(Найти гитару)$'), search))
     dp.add_handler(MessageHandler(Filters.regex('^(Присылать новости)$'), news ))
     dp.add_handler(MessageHandler(Filters.regex('^(Ждать гитару мечты)$'), dream ))
+    dp.add_handler(MessageHandler(Filters.regex('^(Далее)$'), search_next))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     logging.info("Бот стартовал")
     mybot.start_polling()
