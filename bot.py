82d02f2b139f6327569a4e9c7e77e52faa142dc7
VANfrix <ultrafrost2012@yandex.ru>
2022-03-26 19:11:36 +0300

    Доработка по парсингу+кнопки Далее

--- bot.py
+++ bot.py
@@ -14,17 +14,31 @@ def start(update: Update, context: CallbackContext):
     text_greeting = "Привет, я бот для поиска объявлений по продаже гитар!\nЧто я умею:\n- искать гитару по определённым характеристикам\n- присылать новые объявления, нужно только меня об этом попросить)))\n- если ждёшь гитару мечты, я пришлю обявление, когда появится!"
     update.message.reply_text(text_greeting, reply_markup=my_keyboard)
 
-def search(update: Update, context: CallbackContext):
+def get_guitar_message(pages):
     guitars = get_avito()
     dict_list = ''
-    for guitar in guitars:
+    for guitar in guitars[pages:pages+10]:
         name_=guitar['name_guitar']
         price_=guitar['price_guitar']
         saler_=guitar['name_saler']
-        search_new = f'{name_} цена {price_} продаёт {saler_}\n'
+        place_=guitar['place_guitar']
+        search_new = f'\t{name_}\n цена {price_}\n продаёт {saler_}\n место {place_}\n\n'
         dict_list += search_new
-    print(len(dict_list))
-    update.message.reply_text(dict_list)
+    return dict_list
+
+def search(update: Update, context: CallbackContext):
+    pages = 0
+    dict_list = get_guitar_message(pages)
+    # print(len(dict_list))
+    context.user_data["pages"] = pages+10
+    update.message.reply_text(dict_list, reply_markup= ReplyKeyboardMarkup([['Далее']]))
+
+def search_next(update: Update, context: CallbackContext):
+    pages = context.user_data["pages"]
+    dict_list = get_guitar_message(pages)
+    # print(len(dict_list))
+    context.user_data["pages"] = pages+10
+    update.message.reply_text(dict_list, reply_markup= ReplyKeyboardMarkup([['Далее']]))
     
     
 def news(update: Update, context: CallbackContext):
@@ -46,6 +60,7 @@ def main():
     dp.add_handler(MessageHandler(Filters.regex('^(Найти гитару)$'), search))
     dp.add_handler(MessageHandler(Filters.regex('^(Присылать новости)$'), news ))
     dp.add_handler(MessageHandler(Filters.regex('^(Ждать гитару мечты)$'), dream ))
+    dp.add_handler(MessageHandler(Filters.regex('^(Далее)$'), search_next))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     logging.info("Бот стартовал")
     mybot.start_polling()
