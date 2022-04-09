64eabfad94c934faa591c62af4a851faeefe7e68
VANfrix <ultrafrost2012@yandex.ru>
2022-04-09 14:14:33 +0300

    Попытка запустить поиск, ошибки

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from token_bot import bot_token
 from telegram import KeyboardButton, ReplyKeyboardMarkup, Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, Filters, ConversationHandler
 from avito import get_avito
-from model_search import invite_model_search
+from model_search_gitar import invite_model_search, search_name
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 
@@ -11,11 +11,6 @@ logging.basicConfig(filename='bot.log', level=logging.INFO)
 def start(update: Update, context: CallbackContext):
     text_greeting = "Привет, я бот для поиска объявлений по продаже гитар!\nЧто я умею:\n- искать гитару по определённым характеристикам\n- присылать новые объявления, нужно только меня об этом попросить)))\n- если ждёшь гитару мечты, я пришлю обявление, когда появится!"
     update.message.reply_text(text_greeting, reply_markup=main_keyboard())
-
-def main_keyboard():
-    return ReplyKeyboardMarkup([
-        ['Показать гитары', 'Присылать новости', 'Ждать гитару мечты', 'Начать поиск']
-    ])
 
 def get_guitar_message(pages, search_string):
     guitars = get_avito(search_string)
@@ -31,7 +26,7 @@ def get_guitar_message(pages, search_string):
 
 def search(update: Update, context: CallbackContext):
     pages = 0
-    dict_list = get_guitar_message(pages, 'электрогитара')
+    dict_list = get_guitar_message(pages)
     # print(len(dict_list))
     context.user_data["pages"] = pages+10
     update.message.reply_text(dict_list, reply_markup= ReplyKeyboardMarkup([['Далее', 'В основное меню']]))
@@ -43,35 +38,45 @@ def search_next(update: Update, context: CallbackContext):
     context.user_data["pages"] = pages+10
     update.message.reply_text(dict_list, reply_markup= ReplyKeyboardMarkup([['Далее', 'В основное меню']]))
     
-    
 def news(update: Update, context: CallbackContext):
-    update.message.reply_text('Здесь я буду присылать новые объявления')
+    update.message.reply_text('Здесь я буду присылать новые объявления', reply_markup=main_keyboard())
 
 def dream(update: Update, context: CallbackContext):
-    update.message.reply_text('Здесь я пришлю объявление с гитарой твоей мечты')
+    update.message.reply_text('Здесь я пришлю объявление с гитарой твоей мечты', reply_markup=main_keyboard())
+
+def main_keyboard():
+    return ReplyKeyboardMarkup
 
 def search_by_model(update, context):
     text = update.message.text
     dict_list = get_guitar_message(0, text)
     update.message.reply_text(dict_list)
 
+def main_keyboard():
+    return ReplyKeyboardMarkup([
+        ['Показать гитары', 'Присылать новости', 'Ждать гитару мечты', 'Начать поиск']
+    ])
+
 def main():
     mybot = Updater (bot_token, use_context=True)
 
     dp = mybot.dispatcher
-    # model_search = ConversationHandler(
-    #     entry_points=[
-    #         MessageHandler(Filters.regex('^(Начать поиск)$'), invite_model_search)],
-    #     states={},
-    #     fallbacks=[]
-    #     # states={"guitar_name": [MessageHandler(Filters.text, ?????)]}
-    # )
-    # dp.add_handler(model_search)
-    dp.add_handler(CommandHandler("start", start )) 
+
+    model_search = ConversationHandler(
+        entry_points=[
+            MessageHandler(Filters.regex('^(Начать поиск)$'), invite_model_search)
+            ],
+        states={
+            "model":[MessageHandler(Filters.text, search_name)]
+        },
+        fallbacks=[]
+    )
+    dp.add_handler(model_search)
+    dp.add_handler(CommandHandler("start", start)) 
     dp.add_handler(MessageHandler(Filters.regex('^(Показать гитары)$'), search))
     dp.add_handler(MessageHandler(Filters.regex('^(Присылать новости)$'), news ))
     dp.add_handler(MessageHandler(Filters.regex('^(Ждать гитару мечты)$'), dream ))
-    dp.add_handler(MessageHandler(Filters.regex('^(Далее)$'), search_next))
+    # dp.add_handler(MessageHandler(Filters.regex('^(Далее)$'), search_next))
     dp.add_handler(MessageHandler(Filters.regex('^(В основное меню)$'), main_keyboard))
     
     logging.info("Бот стартовал")
